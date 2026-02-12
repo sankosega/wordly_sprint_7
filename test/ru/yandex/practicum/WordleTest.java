9663b0e285f3bf02c762b7d660be1256a6b980ba
sankosega <sarkisiana@internet.ru>
2026-02-12 14:37:23 +0300

    Игра Wordly. Согласно ТЗ

--- test/ru/yandex/practicum/WordleTest.java
+++ test/ru/yandex/practicum/WordleTest.java
@@ -1,7 +1,15 @@
 package ru.yandex.practicum;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordleTest {
 
+    @Test
+    void testMainClassExists() {
+        assertDoesNotThrow(() -> {
+            Class.forName("ru.yandex.practicum.Wordle");
+        });
+    }
 }
